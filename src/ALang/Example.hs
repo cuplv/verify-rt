06883e762c1d8b1dfee0635d4d456ca6f45f51e1
octalsrc <nicklewchenko92@gmail.com>
2022-09-01 10:58:44 -0600

    More comments

--- src/ALang/Example.hs
+++ src/ALang/Example.hs
@@ -3,10 +3,6 @@ module ALang.Example where
 import ALang.Prelude
 import Data.SBV
 
-{-| The state remains non-negative, as an invariant. -}
-nonNegative :: (Avs a, Avs b) => TSpec Int a b
-nonNegative = invarS $ \d1 d2 -> (d1 .>= 0) .=> (d2 .>= 0)
-
 {-| Subtract the input amount from the state, if the amount is
   available. -}
 takeStock :: ALang Int Int (Either String Int)
@@ -31,3 +27,31 @@ test = do
   r1 <- takeStock `checkSpec` nonNegative
   r2 <- badTakeStock `checkSpec` nonNegative
   return $ r1 && not r2
+
+{-| The state remains non-negative, as an invariant. -}
+nonNegative :: (Avs a, Avs b) => TSpec Int a b
+nonNegative = invarS $ \d1 d2 -> (d1 .>= 0) .=> (d2 .>= 0)
+
+{-| Add the given 'Int' to the input value. -}
+plus :: (Avs d) => Int -> ALang d Int Int 
+plus n = Split >>> ATimes (Const n) Id >>> VdTerm Sum
+
+{-| Apply a Left/Right term to the input value, and then put the input
+  value into the Left or Right result. -}
+iteA
+  :: (Avs d, Avs a, Avs b1, Avs b2)
+  => ALang d a (Either b1 b2)
+  -> ALang d a (Either a a)
+iteA a = Split >>> secondA a >>> DistL >>> ASum TakeL TakeL
+
+-- The following convenience terms wrap primitive IntSV terms defined
+-- in ./src/Service.hs
+
+effectAdd :: ALang Int Int Int
+effectAdd = SvTerm SvAdd
+
+effectSub :: ALang Int Int Int
+effectSub = VdTerm Negate >>> SvTerm SvAdd >>> VdTerm Negate
+
+queryAtLeast :: ALang Int Int (Either () Int)
+queryAtLeast = SvTerm SvGE
--- src/ALang/Prelude.hs
+++ src/ALang/Prelude.hs
@@ -3,33 +3,9 @@ module ALang.Prelude
   , module Service
   , module Symbol
   , module ValDomain
-
-  , plus
-  , effectAdd
-  , effectSub
-  , queryAtLeast
-  , iteA
   ) where
 
 import ALang
 import Service
 import Symbol
 import ValDomain
-
-plus :: (Avs d) => Int -> ALang d Int Int 
-plus n = Split >>> ATimes (Const n) Id >>> VdTerm Sum
-
-effectAdd :: ALang Int Int Int
-effectAdd = SvTerm SvAdd
-
-effectSub :: ALang Int Int Int
-effectSub = VdTerm Negate >>> SvTerm SvAdd >>> VdTerm Negate
-
-queryAtLeast :: ALang Int Int (Either () Int)
-queryAtLeast = SvTerm SvGE
-
-iteA
-  :: (Avs d, Avs a, Avs b1, Avs b2)
-  => ALang d a (Either b1 b2)
-  -> ALang d a (Either a a)
-iteA a = Split >>> secondA a >>> DistL >>> ASum TakeL TakeL

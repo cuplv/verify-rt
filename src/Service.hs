f0e2ce34b56c33e7b773d1b06dd1f17013f404c2
octalsrc <nicklewchenko92@gmail.com>
2022-09-15 10:49:39 -0600

    more wip...

--- src/Service.hs
+++ src/Service.hs
@@ -19,6 +19,12 @@ import qualified Data.SBV.List as SList
 data Service s a
   = Service (ALang a (SReq s, SEff s))
 
+data ReadService s a
+  = ReadService { rsReq :: ALang (Ab a) (SReq s) }
+
+data WriteService s a
+  = WriteService (ALang a (SEff s))
+
 data SLang s a b where
   SlPure :: ALang a b -> SLang s a b
   SlService :: Service s a -> SLang s a (Either () s)

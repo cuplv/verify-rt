940e735bb45a91c3dfe8bb18e5e2565f53946b6b
octalsrc <nicklewchenko92@gmail.com>
2022-10-19 13:18:24 -0600

    Fill in MaybeMap terms

---
+++ src/Store/Model/Maybe.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Store.Model.Maybe where
+
+import ALang
+import ALang.Construct
+import Symbol
+import Store.Model
+
+import Data.SBV
+import Data.SBV.Tuple
+import qualified Data.SBV.Either as SE
+import qualified Data.SBV.Maybe as SM
+
+data MaybeUpd a u
+  = MaybeUpd (Maybe (Either a u))
+  deriving (Show,Eq,Ord)
+
+type U u = MaybeUpd (UState u) u
+
+instance (Avs a, Avs u) => Avs (MaybeUpd a u) where
+  type Rep (MaybeUpd a u) = Maybe (Either (Rep a) (Rep u))
+  toRep (MaybeUpd a) = toRep a
+  repc = undefined
+
+instance (Avs a, Avs u) => AData (MaybeUpd a u) where
+  type Content (MaybeUpd a u) = Maybe (Either a u)
+  conA = ArrF return MaybeUpd
+  deconA = ArrF return (\(MaybeUpd a) -> a)
+
+instance (Update u, UState u ~ a) => Update (MaybeUpd a u) where
+  type UState (MaybeUpd a u) = Maybe a
+  idU = MaybeUpd (Just (Right idU))
+  seqU = undefined
+  applyU = undefined
+  symU = undefined
--- src/Store/Model/MaybeMap.hs
+++ src/Store/Model/MaybeMap.hs
@@ -47,6 +47,11 @@ instance (Avs a, Avs b) => AData (Val a b) where
                    (tuple (sNothing, v2)))
     (\(Val a b) -> (a,b))
 
+-- data ValUpd a b = ValUpd (Maybe a, b) deriving (Show,Eq,Ord)
+
+-- instance (Avs a, Avs b) => Avs (ValUpd a b) where
+--   type Rep (ValUpd a b) = SMap.F
+
 data Map a b = Map (Map.Map Int (Val a b))
 
 instance Avs (Map a b) where
@@ -55,16 +60,98 @@ instance Avs (Map a b) where
                 | otherwise = forall_
   repc = undefined
 
-data Upd a b
+data Action a b
   = Insert Key (Val a b)
   | Delete Key
   deriving (Show,Eq,Ord)
+
+data Upd a b = Upd (Map.Map Key (Action a b)) deriving (Show,Eq,Ord)
 
 instance Avs (Upd a b) where
   type Rep (Upd a b) = SMap.U
   toRep = undefined
   repc = undefined
 
+instance Update (Upd a b) where
+  type UState (Upd a b) = Map a b
+  idU = Upd Map.empty
+  seqU _ = ArrF
+    (\a -> pure $ SMap.seq (_1 a) (_2 a))
+    undefined
+  applyU _ = ArrF
+    (\a -> pure $ SMap.update (_1 a) (_2 a))
+    undefined
+
+insert :: (Avs a, Avs b) => ALang t (Key, Val a b) (Upd a b)
+insert = ArrF
+  (\a -> pure $ SMap.insert (_1 a) (_2 a))
+  undefined
+
+insertE 
+  :: (Avs a, Avs b, Avs c) 
+  => ALang t a Key 
+  -> ALang t a (Val b c)
+  -> ALang t a (Upd b c)
+insertE = eform2 insert
+
+delete :: (Avs a, Avs b) => ALang t Key (Upd a b)
+delete = ArrF
+  (\a -> pure $ SMap.delete a)
+  undefined
+
+deleteE 
+  :: (Avs a, Avs b, Avs c) 
+  => ALang t a Key 
+  -> ALang t a (Upd b c)
+deleteE = eform delete
+
 -- We don't actually want AData, since we have multiple constructors (insert, modify, delete) and we don't care about deconstructing.
 
 -- instance AData (Upd a b) where
+
+data G1 a b
+  = G1 (Maybe Key)
+  deriving (Show,Eq,Ord)
+
+instance Avs (G1 a b) where
+  type Rep (G1 a b) = Maybe (Rep Key)
+
+instance AData (G1 a b) where
+  type Content (G1 a b) = Maybe Key
+  conA = ArrF return G1
+  deconA = ArrF return (\(G1 a) -> a)
+
+instance Grant (G1 a b) where
+  type GUpd (G1 a b) = Upd a b
+  readG _ g s1 s2 = do
+    return $ Data.SBV.Maybe.maybe
+      sTrue
+      (\k -> SMap.match k s1 s2)
+      g
+  writeG _ g s1 s2 = do
+    k1 <- forall_
+    return $ Data.SBV.Maybe.maybe
+      (s1 .== s2)
+      (\k -> (k ./= k1) .=> SMap.match k1 s1 s2)
+      g
+
+memberE 
+  :: (Avs a, Avs c, Avs b) 
+  => ALang t a Key 
+  -> ALang t a (Map c b) 
+  -> ALang t a Bool
+memberE = eform2 $ ArrF
+  (\a -> pure $ SMap.member (_1 a) (_2 a))
+  undefined
+
+lookupE
+  :: (Avs a, Avs c, Avs b)
+  => ALang t a Key
+  -> ALang t a (Map b c)
+  -> ALang t a (Val b c)
+lookupE = eform2 $ ArrF
+  (\a -> do
+    v <- forall_
+    constrain $ SMap.hasVal (_1 a) v (_2 a)
+    return v)
+  undefined
